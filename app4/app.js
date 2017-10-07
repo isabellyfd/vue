fc1468d0b756f5573a4146d103bb3e1a5bc57f8f
Isabelly <icfd@cin.ufpe.br>
2017-10-07 14:31:25 -0300

    App 4

---
+++ app4/app.js
@@ -0,0 +1,10 @@
+var app4 = new Vue({
+    el: '#app-4',
+    data: {
+        todos: [
+            {text:  'Hi this this and that'}, 
+            {text: 'Let\' cirque every day'}, 
+            {text: 'Tantan tatanrarana rnanran '}
+        ]
+    }
+})
